02a0ad2b77977b2503ab62fe316b82bfd6ff36ab
kirill.klimonov <kklimonov@yandex.ru>
2023-07-19 17:06:53 +0300

    Added second microservice

---
+++ MC_Customer/src/test/java/com/sdi/klimonov/McCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdi.klimonov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
