779ae3c6f6f9abcafc2e1a84e333f6d62108148b
kirill.klimonov <kklimonov@yandex.ru>
2023-07-20 14:06:11 +0300

    Added all controllers. Both of ms ready for using

--- MC_Customer/src/main/java/com/sdi/klimonov/exceptions/ExceptionTranslator.java
+++ MC_Customer/src/main/java/com/sdi/klimonov/exceptions/ExceptionTranslator.java
@@ -25,10 +25,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
-/**
- * Controller advice to translate the server side exceptions to client-friendly json structures.
- * The error response follows RFC7807 - Problem Details for HTTP APIs (https://tools.ietf.org/html/rfc7807).
- */
+
 @ControllerAdvice
 public class ExceptionTranslator implements ProblemHandling, AdviceTrait {
 
